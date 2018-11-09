b64e498143181f3891a46155c3914ac642656057
Fuli33 <44379088+Fuli33@users.noreply.github.com>
2018-11-09 11:52:35 +0300

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame(,لقد تعلمت أن هناك شيئا بنّاء يأتي من كل هزيمة"~َ. ,,'https://www.twitch.tv/MeeRcY%27)
+
+});
+client.login(process.env.BOT_TOKEN);
